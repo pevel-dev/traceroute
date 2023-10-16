2380f32b3619b1b09e4fb59d8cded73aec977b5c
pevel-dev <pavelshemetov@doubletapp.io>
2023-10-16 22:25:57 +0000

    traceroute ipv4

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import argparse
 
-from traceroute import traceroute
-from utils import get_ip_from_host
+from traceroute import Traceroute
+from utils import get_ip_from_host, get_host_from_ip
 
 parser = argparse.ArgumentParser(description="traceroute")
 parser.add_argument('--host', type=str, help='Host', default="google.com")
 parser.add_argument('--ttl', type=int, help='Max ttl', default=30)
-parser.add_argument('--timeout', type=int, help='Timeout in seconds', default=2)
-
+parser.add_argument('--timeout', type=float, help='Timeout float', default=2.0)
+parser.add_argument('--seq', type=int, help='Seq num', default=0)
+parser.add_argument('--size', type=int, help='Num of bytes size', default=60)
+parser.add_argument('-n', type=int, help='Count packets send', default=5)
 
 if __name__ == "__main__":
     args = parser.parse_args()
@@ -15,5 +17,22 @@ if __name__ == "__main__":
     destination_ip = get_ip_from_host(args.host)
     if not destination_ip:
         print("Not found host")
-
-    traceroute(destination_ip, args.ttl, args.timeout)
+        quit(0)
+    print(f'Traceroute to {args.host} ({destination_ip}). Send {args.n} packets for {args.size} bytes each')
+    i = 1
+    for result in Traceroute().traceroute(destination_ip, args.ttl, args.n, args.timeout, args.size, args.seq):
+        out = [f'{i}:']
+        ips = set()
+        for r in result:
+            if r:
+                ip, time = r
+                if ip in ips:
+                    continue
+                ips.add(ip)
+                out.append(f"{ip} ({get_host_from_ip(ip)}) {round(time, 3)} ms")
+            else:
+                out.append("*")
+        print(' '.join(out))
+        if destination_ip in ips:
+            print('Достигнут конечный узел.')
+        i += 1
--- traceroute.py
+++ traceroute.py
@@ -1,20 +1,78 @@
+import socket
+import struct
 import time
 
-from icmp import send_icmp_packet, receive_icmp_response
-from utils import get_host_from_ip
 
+class Traceroute:
+    def traceroute(self, destination, max_hops, n, timeout=5, size=1, sequence_number=0):
+        for ttl in range(1, max_hops + 1):
+            results = []
+            for i in range(n):
+                results.append(self._task(ttl, size, destination, timeout, sequence_number))
+            yield results
 
-def traceroute(destination_ip, max_ttl, timeout):
-    for ttl in range(1, max_ttl + 1):
-        send_icmp_packet(destination_ip, ttl)
-        response, address = receive_icmp_response(timeout)
-        if address:
-            host = get_host_from_ip(address[0])
-            print(f"{ttl}: {address[0]} ({host})")
-        else:
-            print(f"{ttl}: *")
+            if destination in set(map(lambda x: x[0] if x is not None else " ", results)):
+                return
 
-        if address and address[0] == destination_ip:
-            print("Достигнут конечный узел.")
-            break
-        time.sleep(0.1)
+    def _get_icmp_packet(self, sock, sequence_number, size):
+        icmp_type = 8
+        icmp_code = 0
+        data = b'\x00' * size
+        packet_id = int(int((id(sock) / 65535)) & 0xFFFF)
+
+        temp = struct.pack('!BBHHH', icmp_type, icmp_code, 0, packet_id, sequence_number) + data
+        checksum = self._calculate_checksum(temp)
+
+        return struct.pack('!BBHHH', icmp_type, icmp_code, checksum, packet_id, sequence_number) + data, packet_id
+
+    def _task(self, ttl, size, destination, timeout, sequence_number):
+        sock = self._get_icmp_socket(ttl)
+        packet, packet_id = self._get_icmp_packet(sock, sequence_number, size)
+        start_time = self._send_icmp(sock, packet, destination)
+
+        sock = self._get_receive_socket(timeout)
+        response = self._receive_icmp(sock, packet_id)
+        sock.close()
+
+        if response:
+            return response[0], (response[1] - start_time) * 1000
+        return None
+
+    @staticmethod
+    def _calculate_checksum(data):
+        checksum = 0
+        for i in range(0, len(data), 2):
+            checksum += (data[i] << 8) + data[i + 1]
+        checksum = (checksum >> 16) + (checksum & 0xffff)
+        checksum += (checksum >> 16)
+        return (~checksum) & 0xffff
+
+    @staticmethod
+    def _send_icmp(sock, packet, destination):
+        sock.sendto(packet, (destination, 0))
+        sock.close()
+        return time.time()
+
+    @staticmethod
+    def _receive_icmp(sock, packet_id):
+        while True:
+            try:
+                data, addr = sock.recvfrom(1024)
+                icmp_header = data[20:28]
+                icmp_type, code, checksum, recv_id, sequence = struct.unpack('!BBHHH', icmp_header)
+                if icmp_type == 0 or icmp_type == 11:
+                    return addr[0], time.time()
+            except socket.timeout:
+                return None
+
+    @staticmethod
+    def _get_icmp_socket(ttl):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
+        sock.setsockopt(socket.SOL_IP, socket.IP_TTL, ttl)
+        return sock
+
+    @staticmethod
+    def _get_receive_socket(timeout):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
+        sock.settimeout(timeout)
+        return sock
